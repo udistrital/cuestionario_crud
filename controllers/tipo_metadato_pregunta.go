5eb0b4b62ba1abedc20cc01feb31ae29dab26425
Fabian Barreto <fdbarretos@correo.udistrital.edu.co>
2019-08-07 10:16:42 -0500

    se realiza el refactor como los lineamientos

--- controllers/tipo_metadato_pregunta.go
+++ controllers/tipo_metadato_pregunta.go
@@ -3,11 +3,13 @@ package controllers
 import (
 	"encoding/json"
 	"errors"
-	"github.com/udistrital/cuestionario_crud/models"
 	"strconv"
 	"strings"
 
+	"github.com/udistrital/cuestionario_crud/models"
+
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/logs"
 )
 
 // TipoMetadatoPreguntaController operations for TipoMetadatoPregunta
@@ -29,7 +31,7 @@ func (c *TipoMetadatoPreguntaController) URLMapping() {
 // @Description create TipoMetadatoPregunta
 // @Param	body		body 	models.TipoMetadatoPregunta	true		"body for TipoMetadatoPregunta content"
 // @Success 201 {int} models.TipoMetadatoPregunta
-// @Failure 403 body is empty
+// @Failure 400 the request contains incorrect syntax
 // @router / [post]
 func (c *TipoMetadatoPreguntaController) Post() {
 	var v models.TipoMetadatoPregunta
@@ -38,10 +40,16 @@ func (c *TipoMetadatoPreguntaController) Post() {
 			c.Ctx.Output.SetStatus(201)
 			c.Data["json"] = v
 		} else {
-			c.Data["json"] = err.Error()
+			logs.Error(err)
+			//c.Data["development"] = map[string]interface{}{"Code": "000", "Body": err.Error(), "Type": "error"}
+			c.Data["system"] = err
+			c.Abort("400")
 		}
 	} else {
-		c.Data["json"] = err.Error()
+		logs.Error(err)
+		//c.Data["development"] = map[string]interface{}{"Code": "000", "Body": err.Error(), "Type": "error"}
+		c.Data["system"] = err
+		c.Abort("400")
 	}
 	c.ServeJSON()
 }
@@ -51,14 +59,17 @@ func (c *TipoMetadatoPreguntaController) Post() {
 // @Description get TipoMetadatoPregunta by id
 // @Param	id		path 	string	true		"The key for staticblock"
 // @Success 200 {object} models.TipoMetadatoPregunta
-// @Failure 403 :id is empty
+// @Failure 404 not found resource
 // @router /:id [get]
 func (c *TipoMetadatoPreguntaController) GetOne() {
 	idStr := c.Ctx.Input.Param(":id")
 	id, _ := strconv.Atoi(idStr)
 	v, err := models.GetTipoMetadatoPreguntaById(id)
 	if err != nil {
-		c.Data["json"] = err.Error()
+		logs.Error(err)
+		//c.Data["development"] = map[string]interface{}{"Code": "000", "Body": err.Error(), "Type": "error"}
+		c.Data["system"] = err
+		c.Abort("404")
 	} else {
 		c.Data["json"] = v
 	}
@@ -75,7 +86,7 @@ func (c *TipoMetadatoPreguntaController) GetOne() {
 // @Param	limit	query	string	false	"Limit the size of result set. Must be an integer"
 // @Param	offset	query	string	false	"Start position of result set. Must be an integer"
 // @Success 200 {object} models.TipoMetadatoPregunta
-// @Failure 403
+// @Failure 404 not found resource
 // @router / [get]
 func (c *TipoMetadatoPreguntaController) GetAll() {
 	var fields []string
@@ -121,8 +132,14 @@ func (c *TipoMetadatoPreguntaController) GetAll() {
 
 	l, err := models.GetAllTipoMetadatoPregunta(query, fields, sortby, order, offset, limit)
 	if err != nil {
-		c.Data["json"] = err.Error()
+		logs.Error(err)
+		//c.Data["development"] = map[string]interface{}{"Code": "000", "Body": err.Error(), "Type": "error"}
+		c.Data["system"] = err
+		c.Abort("404")
 	} else {
+		if l == nil {
+			l = append(l, map[string]interface{}{})
+		}
 		c.Data["json"] = l
 	}
 	c.ServeJSON()
@@ -134,7 +151,7 @@ func (c *TipoMetadatoPreguntaController) GetAll() {
 // @Param	id		path 	string	true		"The id you want to update"
 // @Param	body		body 	models.TipoMetadatoPregunta	true		"body for TipoMetadatoPregunta content"
 // @Success 200 {object} models.TipoMetadatoPregunta
-// @Failure 403 :id is not int
+// @Failure 400 the request contains incorrect syntax
 // @router /:id [put]
 func (c *TipoMetadatoPreguntaController) Put() {
 	idStr := c.Ctx.Input.Param(":id")
@@ -142,12 +159,18 @@ func (c *TipoMetadatoPreguntaController) Put() {
 	v := models.TipoMetadatoPregunta{Id: id}
 	if err := json.Unmarshal(c.Ctx.Input.RequestBody, &v); err == nil {
 		if err := models.UpdateTipoMetadatoPreguntaById(&v); err == nil {
-			c.Data["json"] = "OK"
+			c.Data["json"] = v
 		} else {
-			c.Data["json"] = err.Error()
+			logs.Error(err)
+			//c.Data["development"] = map[string]interface{}{"Code": "000", "Body": err.Error(), "Type": "error"}
+			c.Data["system"] = err
+			c.Abort("400")
 		}
 	} else {
-		c.Data["json"] = err.Error()
+		logs.Error(err)
+		//c.Data["development"] = map[string]interface{}{"Code": "000", "Body": err.Error(), "Type": "error"}
+		c.Data["system"] = err
+		c.Abort("400")
 	}
 	c.ServeJSON()
 }
@@ -157,15 +180,18 @@ func (c *TipoMetadatoPreguntaController) Put() {
 // @Description delete the TipoMetadatoPregunta
 // @Param	id		path 	string	true		"The id you want to delete"
 // @Success 200 {string} delete success!
-// @Failure 403 id is empty
+// @Failure 404 not found resource
 // @router /:id [delete]
 func (c *TipoMetadatoPreguntaController) Delete() {
 	idStr := c.Ctx.Input.Param(":id")
 	id, _ := strconv.Atoi(idStr)
 	if err := models.DeleteTipoMetadatoPregunta(id); err == nil {
-		c.Data["json"] = "OK"
+		c.Data["json"] = map[string]interface{}{"Id": id}
 	} else {
-		c.Data["json"] = err.Error()
+		logs.Error(err)
+		//c.Data["development"] = map[string]interface{}{"Code": "000", "Body": err.Error(), "Type": "error"}
+		c.Data["system"] = err
+		c.Abort("404")
 	}
 	c.ServeJSON()
 }
